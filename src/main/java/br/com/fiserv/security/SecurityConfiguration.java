56ac3d14e04e2b608e68ef4674e7c9e52d1e33d7
Lucas Nogueira Mesquita <amcom_lucas@badesc.gov.br>
2022-10-30 13:19:37 -0300

    Keycloak config

--- src/main/java/br/com/fiserv/security/SecurityConfiguration.java
+++ src/main/java/br/com/fiserv/security/SecurityConfiguration.java
@@ -1,44 +1,57 @@
 package br.com.fiserv.security;
 
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
-@Configuration
+@KeycloakConfiguration
 @EnableWebSecurity
-public class SecurityConfiguration {
+@EnableGlobalMethodSecurity(jsr250Enabled = true)
+@Import(KeycloakSpringBootConfigResolver.class)
+public class SecurityConfiguration extends KeycloakWebSecurityConfigurerAdapter {
 		
-    @Bean
-    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-    	
-        http.cors()
-        .and()
-          .authorizeRequests()
-            .antMatchers("/api/employee/**")
-              .hasAuthority("admin")
-            .antMatchers("/swagger.html/**")
-              .permitAll()
-            .antMatchers("/api/**")
-              .authenticated()
-        .and()
-          .oauth2ResourceServer()
-            .jwt()
-              .jwtAuthenticationConverter(getJwtAuthenticationConverter());
-        
-        return http.build();
+    /**
+     * Registers the KeycloakAuthenticationProvider with the authentication manager.
+     */
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    	KeycloakAuthenticationProvider authenticationProvider = new KeycloakAuthenticationProvider();
+    	authenticationProvider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
+        auth.authenticationProvider(authenticationProvider);
     }
-    
-    JwtAuthenticationConverter getJwtAuthenticationConverter() {
-    	JwtGrantedAuthoritiesConverter converter = new JwtGrantedAuthoritiesConverter();
-    	converter.setAuthoritiesClaimName("authorities");
-    	converter.setAuthorityPrefix("");
-    	JwtAuthenticationConverter authenticationConverter = new JwtAuthenticationConverter();
-    	authenticationConverter.setJwtGrantedAuthoritiesConverter(converter);
-    	return authenticationConverter;
+
+    /**
+     * Defines the session authentication strategy.
+     */
+    @Bean
+    @Override
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(buildSessionRegistry());
+    }
+
+    @Bean
+    protected SessionRegistry buildSessionRegistry() {
+        return new SessionRegistryImpl();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception
+    {
+        super.configure(http);
+        http.authorizeRequests().anyRequest().permitAll();
     }
    
 }
